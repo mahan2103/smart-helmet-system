05d6337ee8038e51d471c5237e4121cba4c43fc3
mahan2103 <mahan.hs2103@gmail.com>
2026-05-20 10:15:14 +0200

    Create sensors.cpp

---
+++ sensors.cpp
@@ -0,0 +1,11 @@
+
+#include "sensors.h"
+#include <cstdlib>
+
+bool Sensors::isHelmetWorn() {
+    return rand() % 2;  // 0 = not worn, 1 = worn
+}
+
+bool Sensors::detectImpact() {
+    return rand() % 2;  // simulate accident detection
+}
